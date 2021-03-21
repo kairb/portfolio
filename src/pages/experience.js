90f74250a7b1f025f4e44a7aa3849b1c26ba61dd
Kai Roper-Blackman <Kairoperblackman@gmail.com>
2021-03-21 21:36:12 +0000

    import issues

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useStaticQuery } from 'gatsby';
 import SEO from '../components/seo';
-import { Project } from '../components/Project';
+import Project from '../components/Project';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
