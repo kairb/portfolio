1f05611966fcac41a0baf978671050561fa91282
Kai Roper-Blackman <Kairoperblackman@gmail.com>
2021-06-06 16:55:05 +0100

    removed deprecated code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,9 +8,3 @@
 import ReactDOM from 'react-dom';
 
 export { wrapRootElement } from './wrapRootElement';
-
-export function replaceHydrateFunction() {
-  return (element, container, callback) => {
-    ReactDOM.render(element, container, callback);
-  };
-}
