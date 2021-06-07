3acee76695fe0700e3c147d3f84129fcadf946be
Kai Roper-Blackman <Kairoperblackman@gmail.com>
2021-06-07 23:21:09 +0100

    added pepe 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,48 @@
-import React from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { getContentByID } from '../utils/contentfulAPI';
 
-const NotFoundPage = () => (
-  <>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '97vh',
+  },
+  pepe: {
+    width: '300px',
+    borderRadius: '25%',
+  },
+}));
+const NotFoundPage = ({ data }) => {
+  const classes = useStyles();
+  const {
+    message,
+    image: {
+      fields: {
+        description,
+        file: { url },
+      },
+    },
+  } = data;
+  console.log(data);
+  return (
+    <div className={classes.root}>
+      <img className={classes.pepe} src={url} alt={description} />
+      <h1>{message}</h1>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export async function getStaticProps() {
+  const data = await getContentByID('1lvghkZDqrIdSP039dDxuF');
+  return {
+    props: {
+      data: data.fields,
+    },
+  };
+}
+
+export default NotFoundPage;
