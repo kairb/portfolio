bff06d7ea47676523e3ba88c1f355cc2f8fa425b
Kai Roper-Blackman <Kairoperblackman@gmail.com>
2021-06-10 23:37:45 +0100

    added new colours to theme

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
-    primary: { 500: '#467fcf' },
+    background: '#E9EDEE',
     buttonColor: '#CBC3E3',
     formValidationError: '#ff0000',
+    blue: '#4a86e8',
   },
 });
 export default theme;
