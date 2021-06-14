c524e005ab9d7894b18f3da4fadbb26fa8eb9f9f
Kai Roper-Blackman <Kairoperblackman@gmail.com>
2021-06-14 09:50:07 +0100

    simple blog implementation

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,7 +1,83 @@
-import { useRouter } from 'next/router';
+import { makeStyles } from '@material-ui/core/styles';
+import { getAllContentByID, getContentByID } from '../../utils/contentfulAPI';
+const fs = require('fs');
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    padding: '20px',
+  },
+}));
+const BlogPost = ({ data }) => {
+  const classes = useStyles();
 
-const BlogPost = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  return <div>blog post {slug}</div>;
+  const {
+    sys: { slug, createdAt },
+    fields: { title, shortDescription },
+  } = data;
+  // const router = useRouter();
+  // const { slug } = router.query;
+  const date = new Date(createdAt);
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return (
+    <div className={classes.root}>
+      <h1>{title}</h1>
+      <h2>{shortDescription}</h2>
+      <h3>{`By Kai Roper-Blackman, ${
+        monthNames[date.getMonth()]
+      } ${date.getFullYear()}`}</h3>
+    </div>
+  );
+};
+
+export default BlogPost;
+
+export const getStaticProps = async ({ params }) => {
+  const pageMappings = JSON.parse(fs.readFileSync('./pages.json'));
+  let id;
+  pageMappings.forEach(page => {
+    if (page.params.slug === params.slug) {
+      id = page.params.id;
+    }
+  });
+  const data = await getContentByID(id);
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
+export const getStaticPaths = async () => {
+  const data = await getAllContentByID('blogPost');
+  const paths = data.items.map(item => {
+    const {
+      sys: { id },
+      fields: { slug },
+    } = item;
+    return {
+      params: {
+        id: id,
+        slug: slug,
+      },
+    };
+  });
+  fs.writeFileSync('./pages.json', JSON.stringify(paths));
+  return {
+    paths: paths,
+    fallback: false,
+  };
 };
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { getAllContentByID } from '../../utils/contentfulAPI';
+import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    padding: '20px',
+  },
+}));
 
-const Index = () => {
+const Index = ({ data }) => {
+  const classes = useStyles();
   return (
-    <>
+    <div className={classes.root}>
       <h1>All my blog posts</h1>
-    </>
+      {data.items.map(blogPost => {
+        const { slug, title } = blogPost.fields;
+        return (
+          <Link key={slug} href={`/blog/${slug}`}>
+            {title}
+          </Link>
+        );
+      })}
+    </div>
   );
 };
 
 export default Index;
+
+export const getStaticProps = async () => {
+  const data = await getAllContentByID('blogPost');
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
